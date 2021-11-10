01276fbd781128b1f232101ecf3ae0569e57e1b0
3LVN <59151135+3LVN@users.noreply.github.com>
2021-11-10 12:47:03 +0700

    Update PhanTu.java

--- PhanTu.java
+++ PhanTu.java
@@ -2,10 +2,18 @@ public class PhanTu<T extends Comparable<T>> {
 
     private T t;
 
+    public T getT() {
+        return this.t;
+    }
+
+    public void setT(T tee) {
+        this.t = tee;
+    }
+    
     public boolean isGreaterThan(T a) {
         return t.compareTo(a) > 0;
     }
-    
+
 
 
 }
